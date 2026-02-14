996edda8407f297647af9cd0d1cef94a6896d36b
Simeon Rediger <simeonrediger@gmail.com>
2026-02-14 17:31:38 -0500

    Extract section styles into separate files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './styles/reset.css';
 import './styles/colors.css';
 import './styles/layout.css';
+import './styles/about-me.css';
+import './styles/projects.css';
+import './styles/contact.css';
