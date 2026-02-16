d7113a717a5e54cb9bbe9d3f4fbcb27d9632dc83
Simeon Rediger <simeonrediger@gmail.com>
2026-02-16 18:21:12 -0500

    Add whitespace

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './styles/reset.css';
+
 import './styles/colors.css';
 import './styles/layout.css';
+
 import './styles/about-me.css';
 import './styles/projects.css';
 import './styles/contact.css';
+
 import './styles/icon.css';
 import './styles/utilities.css';
